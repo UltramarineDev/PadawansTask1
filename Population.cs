bb112f1f11418a52957469993da48e941377bac0
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-02-14 00:52:36 +0300

    Initial Solution

--- Population.cs
+++ Population.cs
@@ -6,8 +6,28 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
+            int years = 0;
             // put your code here
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            if (initialPopulation > currentPopulation || initialPopulation < 0 || currentPopulation < 0 || visitors < 0 || percent < 0)
+            {
+                throw new ArgumentException();
+            }
+           double ninitialPopulation = initialPopulation;
+
+           while (ninitialPopulation <= currentPopulation)
+            {
+                try
+                {
+                     ninitialPopulation = ninitialPopulation + ninitialPopulation * (percent / 100) + visitors;
+                     years++;
+                }
+               catch(OverflowException)
+                {
+                    throw new OverflowException();
+                }
+            }
+            return years;
         }
     }
 }
